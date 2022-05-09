cd993e6d1770f85cd7f9e76001a575a22b819f4f
Jhonathan Meireles de Oliveira <jhon4than_jf@hotmail.com>
2022-05-09 18:37:02 -0300

    Calculadora em cSharp

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -10,6 +10,75 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+        inicio:
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Clear();
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Operação");
+            Console.WriteLine("Digite + (soma) - (Subtração) x (Multiplicação) / (Divisão): ");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o segundo número");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado =0;
+
+            if (operacao == "+")
+            {
+                resultado = num1 + num2;
+            }
+            else if (operacao == "-")
+            {
+                resultado = num1 - num2;
+            }
+            else if(operacao == "x")
+            {
+                resultado = num1 * num2;
+            }
+            else if(operacao == "/")
+            {
+                resultado = num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("Ocorreu um erro na operação");
+            }
+
+            /*
+            switch (operacao)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+
+                case "x":
+                    resultado = num1 * num2;
+                    break;
+
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+                default :
+                    Console.WriteLine("Erro na operação!");
+                    break;
+            }
+            */
+
+            Console.WriteLine("{0} {1} {2} = {3}", num1, operacao, num2, resultado);
+
+            Console.WriteLine("Digite uma tecla para voltar ao início");
+
+            Console.ReadKey();
+
+            goto inicio; //Voltar para o início
         }
     }
 }
